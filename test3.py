f3263ee3caf7feaead8032ec66dda061f0275d42
Brunini <brunini2@hotmail.com>
2020-09-28 16:13:17 +0200

    First commit, test3.py currently not working,  set to ignorefoldx.exe (possibly globally)

---
+++ test3.py
@@ -0,0 +1,16 @@
+import time
+#using subprocess module
+import subprocess
+import os
+#pyautogui for typing??     check this further
+#import pyautogui
+#rC:\Users\38598\Downloads\yasara\YASARA.exe   (r to make the string raw)
+p1 = os.system("foldx.exe ")
+time.sleep(2)
+#yay this part works albeit slowly because yasara is written in potatospeak
+
+#print(p1.stdout)
+#
+#yasara commands:
+#        LoadPDB C:\...\.pdb
+#pyautogui.typewrite("brunoni babe",interval=0.25)
